9b10221a7d8c9115bbb8118c512fcce5a414ded0
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2019-01-28 14:09:18 -0500

    Boom

--- src/main/java/com/codedifferently/thunderborg/Thunderborg.java
+++ src/main/java/com/codedifferently/thunderborg/Thunderborg.java
@@ -32,7 +32,7 @@ public class Thunderborg {
         }
     }*/
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws InterruptedException{
         I2CBus I2C_BUS;
         I2CDevice Thunderborg;
         System.out.println("Hello pi");
@@ -43,11 +43,11 @@ public class Thunderborg {
             System.out.println("Connected to bus. Ok");
             Thunderborg = I2C_BUS.getDevice(i2cAddress);
             System.out.println("Device id " + Thunderborg.read(Constants.COMMAND_GET_ID));
-            if (Constants.I2C_ID_THUNDERBORG  == Thunderborg.read(Constants.COMMAND_GET_ID)){
-                System.out.println("Found Thunderborg");
-            } else {
-                System.out.println("no Thunderborg");
-            }
+            Thunderborg.write(Constants.COMMAND_SET_A_FWD, (byte) Constants.PWM_MAX);
+            System.out.println("Motor on");
+            Thread.sleep(1000l);
+            Thunderborg.write(Constants.COMMAND_SET_A_FWD, (byte) 0);
+
         }catch(IOException exception){
             System.out.println("Failed");
         }
